24130df41046ea22e409b716450a737fa8c59437
rayssa0102 <162484474+rayssa0102@users.noreply.github.com>
2024-10-09 14:54:48 -0300

    09/10

---
+++ graficos/quantidadeusuario.js
@@ -0,0 +1,9 @@
+imort{getCCS, tickCofig} from "./common.js"
+
+async function quantidadedeUsuarioporrede() {
+    cont url ='https://raw.githubusercontent.comhttps://github.com/rayssa0102/HISTORIA-/'
+    cont res= await fetch(url)
+    cont dados = await res.Json()
+    cont nomeDasRedes=
+    
+}
